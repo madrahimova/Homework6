e04b77693b5246a09c96c97eab8ae96626c61623
madrahimova <darya.mad.ry@gmail.com>
2023-03-26 18:59:39 +0300

    Homework 6 completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,94 @@
 public class Main {
+    public static void call(Class cls, String methodName) throws Exception {
+        cls.getDeclaredMethod(methodName).invoke(null);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var tasksCount = 10;
+        for (var i = 1; i <= tasksCount; i++) {
+            try {
+                call(Main.class, "task" + i);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        for (int i = 0; i <= 17; i += 2) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task5() {
+        System.out.println("Задача 5");
+        for (int i = 1904; i <= 2096; i += 4) {
+            System.out.println(i + " год является високосным");
+        }
+    }
+
+    public static void task6() {
+        System.out.println("Задача 6");
+        for (int i = 7; i <= 98; i += 7) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void task7() {
+        System.out.println("Задача 7");
+        for (int i = 1; i <= 512; i *= 2) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void task8() {
+        System.out.println("Задача 8");
+        int income = 0, deposit = 29000;
+        for (int i = 1; i <= 12; i++) {
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + income + " рублей");
+            income += deposit;
+        }
+        System.out.println("Сумма годовых накоплений " + income);
+    }
+
+    public static void task9() {
+        System.out.println("Задача 9");
+        int income = 0, deposit = 29000;
+        for (int i = 1; i <= 12; i++) {
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + income + " рублей");
+            income += income * 0.01;
+            income += deposit;
+        }
+        System.out.println("Сумма годовых накоплений " + income);
+    }
+    public static void task10() {
+        System.out.println("Задача 10");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("2*" + i + "=" + i * 2);
+        }
     }
 }
